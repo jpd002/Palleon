ac55132eccd25f6bebabe8d85c8565f3286bcaa3
jpd002 <jpd002@e658db38-2ae6-4b1c-8a17-0a8554443f32>
2011-08-14 19:40:08 +0000

    Updated AthenaEngine.h Updated MSVC project.
    git-svn-id: http://svn.purei.org/palleon/trunk@4 e658db38-2ae6-4b1c-8a17-0a8554443f32

--- include/AthenaEngine.h
+++ include/AthenaEngine.h
@@ -1,6 +1,11 @@
 #ifndef _ATHENAENGINE_H_
 #define _ATHENAENGINE_H_
 
+#include "athena/Box2.h"
 #include "athena/GraphicDevice.h"
+#include "athena/Mesh.h"
+#include "athena/Label.h"
+#include "athena/Sprite.h"
+#include "athena/Application.h"
 
 #endif
