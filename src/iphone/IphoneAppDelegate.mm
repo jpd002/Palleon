a96b76716d5d25e552ea1e09f981347fe5805a37
jpd002 <jpd002@e658db38-2ae6-4b1c-8a17-0a8554443f32>
2012-10-23 06:20:25 +0000

    Fixed iOS6 rotation problem.
    git-svn-id: http://svn.purei.org/palleon/trunk@59 e658db38-2ae6-4b1c-8a17-0a8554443f32

--- src/iphone/IphoneAppDelegate.mm
+++ src/iphone/IphoneAppDelegate.mm
@@ -7,70 +7,56 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    // Override point for customization after application launch.
-    
-    CGRect screenBounds = [[UIScreen mainScreen] bounds];
-    self.window = [[UIWindow alloc] initWithFrame: screenBounds];
+	// Override point for customization after application launch.
+	
+	CGRect screenBounds = [[UIScreen mainScreen] bounds];
+	self.window = [[UIWindow alloc] initWithFrame: screenBounds];
 
-    m_mainViewController = [[IphoneViewController alloc] init];
-    [self.window addSubview: m_mainViewController.view];
-    
-    [self.window makeKeyAndVisible];
+	m_mainViewController = [[IphoneViewController alloc] init];
+	self.window.rootViewController = m_mainViewController;
+	
+	[self.window makeKeyAndVisible];
 
-    return YES;
+	return YES;
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application
 {
-    /*
-     Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-     Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-     */
+	
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
-    /*
-     Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-     If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-     */
+	
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
-    /*
-     Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-     */
+	
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
-    /*
-     Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-     */
+	
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
 {
-    /*
-     Called when the application is about to terminate.
-     Save data if appropriate.
-     See also applicationDidEnterBackground:.
-     */
+	
 }
 
 - (void)dealloc
 {
-    [_window release];
-    [super dealloc];
+	[_window release];
+	[super dealloc];
 }
 
 @end
 
 int main(int argc, char *argv[])
 {
-    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-    int retVal = UIApplicationMain(argc, argv, nil, @"IphoneAppDelegate");
-    [pool release];
-    return retVal;
+	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+	int retVal = UIApplicationMain(argc, argv, nil, @"IphoneAppDelegate");
+	[pool release];
+	return retVal;
 }
