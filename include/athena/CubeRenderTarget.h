8cec9d132d43697184f768c7ebf89aa98ee26204
jpd002 <jpd002@e658db38-2ae6-4b1c-8a17-0a8554443f32>
2014-03-17 00:29:03 +0000

    Removed cube face enum from CubeRenderTarget.
    git-svn-id: http://svn.purei.org/palleon/trunk@163 e658db38-2ae6-4b1c-8a17-0a8554443f32

--- include/athena/CubeRenderTarget.h
+++ include/athena/CubeRenderTarget.h
@@ -1,31 +1,17 @@
-#ifndef _CUBERENDERTARGET_H_
-#define _CUBERENDERTARGET_H_
+#pragma once
 
 #include "athena/Texture.h"
 #include "athena/Viewport.h"
 
 namespace Athena
 {
-	enum CUBEMAP_FACE
-	{
-		CUBEMAP_FACE_POSITIVE_X,
-		CUBEMAP_FACE_NEGATIVE_X,
-		CUBEMAP_FACE_POSITIVE_Y,
-		CUBEMAP_FACE_NEGATIVE_Y,
-		CUBEMAP_FACE_POSITIVE_Z,
-		CUBEMAP_FACE_NEGATIVE_Z,
-		CUBEMAP_FACE_MAX
-	};
-
 	class CCubeRenderTarget : public CTexture
 	{
 	public:
 		virtual			~CCubeRenderTarget();
 
-		virtual void	Draw(CUBEMAP_FACE, const ViewportPtr&) = 0;
+		virtual void	Draw(TEXTURE_CUBE_FACE, const ViewportPtr&) = 0;
 	};
 
 	typedef std::shared_ptr<CCubeRenderTarget> CubeRenderTargetPtr;
 }
-
-#endif
