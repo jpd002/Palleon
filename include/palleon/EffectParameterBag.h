c6448dc9e83d1cb66db35d479627b06417ad7b3d
jpd002 <jpd002@e658db38-2ae6-4b1c-8a17-0a8554443f32>
2014-05-01 03:16:09 +0000

    Compilation fix.
    git-svn-id: http://svn.purei.org/palleon/trunk@220 e658db38-2ae6-4b1c-8a17-0a8554443f32

--- include/palleon/EffectParameterBag.h
+++ include/palleon/EffectParameterBag.h
@@ -15,53 +15,53 @@ namespace Palleon
 
 		template <typename ParamType>
 		ParamType GetEffectParamOrDefault(const std::string& paramName, const ParamType& defaultValue, bool mustExist = false) const;
-
-		template <>
-		float GetEffectParamOrDefault<float>(const std::string& paramName, const float& defaultValue, bool mustExist) const
-		{
-			auto effectParam = GetEffectParameter(paramName);
-			assert(!mustExist || !effectParam.IsNull());
-			if(effectParam.IsNull()) return defaultValue;
-			return effectParam.GetScalar();
-		}
-
-		template <>
-		CVector2 GetEffectParamOrDefault<CVector2>(const std::string& paramName, const CVector2& defaultValue, bool mustExist) const
-		{
-			auto effectParam = GetEffectParameter(paramName);
-			assert(!mustExist || !effectParam.IsNull());
-			if(effectParam.IsNull()) return defaultValue;
-			return effectParam.GetVector2();
-		}
-
-		template <>
-		CVector3 GetEffectParamOrDefault<CVector3>(const std::string& paramName, const CVector3& defaultValue, bool mustExist) const
-		{
-			auto effectParam = GetEffectParameter(paramName);
-			assert(!mustExist || !effectParam.IsNull());
-			if(effectParam.IsNull()) return defaultValue;
-			return effectParam.GetVector3();
-		}
-
-		template <>
-		CVector4 GetEffectParamOrDefault<CVector4>(const std::string& paramName, const CVector4& defaultValue, bool mustExist) const
-		{
-			auto effectParam = GetEffectParameter(paramName);
-			assert(!mustExist || !effectParam.IsNull());
-			if(effectParam.IsNull()) return defaultValue;
-			if(effectParam.IsVector3())
-			{
-				return CVector4(effectParam.GetVector3(), 0);
-			}
-			else
-			{
-				return effectParam.GetVector4();
-			}
-		}
-
+		
 	private:
 		typedef std::unordered_map<std::string, CEffectParameter> EffectParameterMap;
 
 		EffectParameterMap			m_effectParameters;
 	};
+	
+	template <>
+	inline float CEffectParameterBag::GetEffectParamOrDefault<float>(const std::string& paramName, const float& defaultValue, bool mustExist) const
+	{
+		auto effectParam = GetEffectParameter(paramName);
+		assert(!mustExist || !effectParam.IsNull());
+		if(effectParam.IsNull()) return defaultValue;
+		return effectParam.GetScalar();
+	}
+	
+	template <>
+	inline CVector2 CEffectParameterBag::GetEffectParamOrDefault<CVector2>(const std::string& paramName, const CVector2& defaultValue, bool mustExist) const
+	{
+		auto effectParam = GetEffectParameter(paramName);
+		assert(!mustExist || !effectParam.IsNull());
+		if(effectParam.IsNull()) return defaultValue;
+		return effectParam.GetVector2();
+	}
+	
+	template <>
+	inline CVector3 CEffectParameterBag::GetEffectParamOrDefault<CVector3>(const std::string& paramName, const CVector3& defaultValue, bool mustExist) const
+	{
+		auto effectParam = GetEffectParameter(paramName);
+		assert(!mustExist || !effectParam.IsNull());
+		if(effectParam.IsNull()) return defaultValue;
+		return effectParam.GetVector3();
+	}
+	
+	template <>
+	inline CVector4 CEffectParameterBag::GetEffectParamOrDefault<CVector4>(const std::string& paramName, const CVector4& defaultValue, bool mustExist) const
+	{
+		auto effectParam = GetEffectParameter(paramName);
+		assert(!mustExist || !effectParam.IsNull());
+		if(effectParam.IsNull()) return defaultValue;
+		if(effectParam.IsVector3())
+		{
+			return CVector4(effectParam.GetVector3(), 0);
+		}
+		else
+		{
+			return effectParam.GetVector4();
+		}
+	}
 };
