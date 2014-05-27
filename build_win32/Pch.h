9ec566cd2318031253660a410c33d6899dbdfe65
jpd002 <jpd002@e658db38-2ae6-4b1c-8a17-0a8554443f32>
2014-05-27 03:02:11 +0000

    Added precompiled header on Win32 and cleaned some build settings.
    git-svn-id: http://svn.purei.org/palleon/trunk@230 e658db38-2ae6-4b1c-8a17-0a8554443f32

---
+++ build_win32/Pch.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <cassert>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <functional>
+#include <memory>
+#include <algorithm>
+
+#include <boost/signals2.hpp>
+
+#include <d3d11.h>
+#include <Windows.h>
+
+#undef near
+#undef far
+
+#include "Types.h"
+#include "Stream.h"
+#include "win32/ComPtr.h"
