8f1155c3693f1319507d905e10a5106838f4fb24
jpd002 <jpd002@e658db38-2ae6-4b1c-8a17-0a8554443f32>
2014-09-19 07:43:21 +0000

    Updated Android build config.
    git-svn-id: http://svn.purei.org/palleon/trunk@302 e658db38-2ae6-4b1c-8a17-0a8554443f32

--- build_android/jni/Android.mk
+++ build_android/jni/Android.mk
@@ -6,14 +6,27 @@ BOOST_PATH			:= /cygdrive/c/Components/boost_trunk/
 FRAMEWORK_PATH		:= /cygdrive/c/Projects/Framework/
 
 LOCAL_MODULE		:= libPalleon
-LOCAL_SRC_FILES		:=	../../src/Box2.cpp \
+LOCAL_SRC_FILES		:=	../../src/android/AndroidActivity.cpp \
+						../../src/android/AndroidGraphicDevice.cpp \
+						../../src/android/AndroidLog.cpp \
+						../../src/android/AndroidResourceManager.cpp \
+						../../src/Box2.cpp \
 						../../src/ButtonBase.cpp \
 						../../src/Camera.cpp \
 						../../src/CubeMesh.cpp \
+						../../src/gles/GlEsEffect.cpp \
+						../../src/gles/GlEsGraphicDevice.cpp \
+						../../src/gles/GlEsShadowMapEffect.cpp \
+						../../src/gles/GlEsTexture.cpp \
+						../../src/gles/GlEsUberEffect.cpp \
+						../../src/gles/GlEsUberEffectGenerator.cpp \
+						../../src/gles/GlEsUberEffectProvider.cpp \
+						../../src/gles/GlEsVertexBuffer.cpp \
 						../../src/GraphicDevice.cpp \
 						../../src/InputManager.cpp \
 						../../src/Label.cpp \
 						../../src/LayoutNode.cpp \
+						../../src/Log.cpp \
 						../../src/Material.cpp \
 						../../src/Mesh.cpp \
 						../../src/NinePatch.cpp \
@@ -26,12 +39,14 @@ LOCAL_SRC_FILES		:=	../../src/Box2.cpp \
 						../../src/resources/SceneDescriptor.cpp \
 						../../src/resources/Package.cpp \
 						../../src/resources/ResourceManager.cpp \
+						../../src/resources/TextureConverters.cpp \
 						../../src/resources/TextureResource.cpp \
 						../../src/Scene.cpp \
 						../../src/SceneNode.cpp \
 						../../src/SphereMesh.cpp \
 						../../src/Sprite.cpp \
 						../../src/SpriteButton.cpp \
+						../../src/Texture.cpp \
 						../../src/TextureLoader.cpp \
 						../../src/VertexBuffer.cpp \
 						../../src/Viewport.cpp \
