e5a1f0c62fc25f1663b821659f2d273edfec1b99
jpd002 <jpd002@e658db38-2ae6-4b1c-8a17-0a8554443f32>
2014-09-24 05:14:16 +0000

    Implemented MakeResourceStream in Win32ResourceManager.
    git-svn-id: http://svn.purei.org/palleon/trunk@308 e658db38-2ae6-4b1c-8a17-0a8554443f32

--- src/win32/Win32ResourceManager.cpp
+++ src/win32/Win32ResourceManager.cpp
@@ -1,5 +1,7 @@
 #include <assert.h>
 #include <string>
+#include "StdStream.h"
+#include "make_unique.h"
 #include "palleon/win32/Win32ResourceManager.h"
 
 #define DATA_BASE	"./data/"
@@ -18,19 +20,25 @@ CWin32ResourceManager::~CWin32ResourceManager()
 
 void CWin32ResourceManager::CreateInstance()
 {
-	assert(m_instance == NULL);
-	if(m_instance != NULL) return;
+	assert(m_instance == nullptr);
+	if(m_instance != nullptr) return;
 	m_instance = new CWin32ResourceManager();
 }
 
 void CWin32ResourceManager::DestroyInstance()
 {
-	assert(m_instance != NULL);
+	assert(m_instance != nullptr);
 	delete m_instance;
-	m_instance = NULL;
+	m_instance = nullptr;
 }
 
 std::string CWin32ResourceManager::MakeResourcePath(const std::string& name) const
 {
 	return std::string(DATA_BASE) + name;
+}
+
+CResourceManager::StreamPtr CWin32ResourceManager::MakeResourceStream(const std::string& name) const
+{
+	auto resourcePath = MakeResourcePath(name);
+	return std::make_unique<Framework::CStdStream>(resourcePath.c_str(), "rb");
 }
