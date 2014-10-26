b51943ef0800ddd41d64b1da550b7ddb2eb61ab7
jpd002 <jpd002@e658db38-2ae6-4b1c-8a17-0a8554443f32>
2014-10-26 02:52:52 +0000

    Fixed OpenGlEsDefs header to properly support GLES3.0 on Android.
    git-svn-id: http://svn.purei.org/palleon/trunk@354 e658db38-2ae6-4b1c-8a17-0a8554443f32

--- include/palleon/gles/OpenGlEsDefs.h
+++ include/palleon/gles/OpenGlEsDefs.h
@@ -1,9 +1,15 @@
 #pragma once
 
-#ifdef __APPLE__
+#include "../PlatformDef.h"
+
+#if defined(PALLEON_IOS)
 #import <OpenGLES/ES2/gl.h>
 #import <OpenGLES/ES2/glext.h>
-#else
+#elif defined(PALLEON_ANDROID)
+#include <GLES3/gl3.h>
+#define GL_GLEXT_PROTOTYPES
+#include <GLES3/gl3ext.h>
+#else	//Emscripten/WebGL
 #include <GLES2/gl2.h>
 #define GL_GLEXT_PROTOTYPES
 #include <GLES2/gl2ext.h>
