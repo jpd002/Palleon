a853306cddb8eba19a6681a38da8d5a24baf17de
Jean-Philip Desjardins <jean-philip.desjardins@polymtl.ca>
2016-09-15 22:28:56 -0400

    Fix ShadowMapping project build.

--- samples/ShadowMapping/build_android/src/main/jni/Android.mk
+++ samples/ShadowMapping/build_android/src/main/jni/Android.mk
@@ -1,6 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
 FRAMEWORK_PATH := $(realpath $(LOCAL_PATH)/../../../../../../../Framework)
+NUANCEUR_PATH  := $(realpath $(LOCAL_PATH)/../../../../../../../Nuanceur)
 PALLEON_PATH   := $(realpath $(LOCAL_PATH)/../../../../../../)
 PROJECT_PATH   := $(realpath $(LOCAL_PATH)/../../../../)
 
@@ -10,6 +11,13 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE			:= libFramework
 LOCAL_SRC_FILES 		:= $(FRAMEWORK_PATH)/build_android/obj/local/$(TARGET_ARCH_ABI)/libFramework.a
+
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE			:= libNuanceur
+LOCAL_SRC_FILES 		:= $(NUANCEUR_PATH)/build_android/obj/local/$(TARGET_ARCH_ABI)/libNuanceur.a
 
 include $(PREBUILT_STATIC_LIBRARY)
 
@@ -28,6 +36,6 @@ LOCAL_SRC_FILES			:=	$(PROJECT_PATH)/src/Application.cpp \
 LOCAL_C_INCLUDES		:= $(BOOST_PATH) $(FRAMEWORK_PATH)/include $(PALLEON_PATH)/include
 LOCAL_CPP_FEATURES		:= exceptions rtti
 LOCAL_LDLIBS 			:= -lz -llog -landroid -lGLESv3 -lEGL
-LOCAL_STATIC_LIBRARIES	:= libPalleon libFramework
+LOCAL_STATIC_LIBRARIES	:= libPalleon libNuanceur libFramework
 
 include $(BUILD_SHARED_LIBRARY)
